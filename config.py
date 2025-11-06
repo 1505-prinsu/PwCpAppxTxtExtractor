7779296d2f30e1d81794cc99eacc418ce411b1b2
1505-prinsu <patelkinjal2308@gmail.com>
2025-11-06 08:10:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
+api_id = "27433400"
+api_hash = "1a286620de5ffe0a7d9b57e604293555"
 bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+auth_users = [6201066540]
